8e73ced2a2aa5a5af67c7ed87858234116cee181
rovonov98 <stariyakk121@gmail.com>
2021-04-20 16:26:02 +0500

    ReactTS webpack 5 template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './assets/scss/App.scss'
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>ReactTS webpack5 template</p>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const { CleanWebpackPlugin }  = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (_, { mode }) => ({
+    entry: {
+        main: path.resolve(__dirname, 'src', 'main.tsx'),
+        // vendor: ['react', 'react-dom']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 
+            mode === 'production'
+            ? '[name].[contenthash].bundle.js'
+            : '[name].js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    mode === 'production'
+                    ? MiniCssExtractPlugin.loader
+                    : 'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            additionalData: '@import "src/assets/scss/global/variables.scss";',
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ]
+    },
+    devServer: {
+        port: 3000
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack ReactTS template',
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 
+                mode === 'production' 
+                ? '[name].[contenthash].bundle.css'
+                : '[name].css'
+        })
+    ],
+})
